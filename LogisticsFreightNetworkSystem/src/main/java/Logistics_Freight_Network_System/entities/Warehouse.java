11b8542a97f1aa0588687852bbb8df7746c0fb89
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:37:09 +0400

    created Warehouse Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Warehouse.java
@@ -0,0 +1,33 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "warehouses")
+public class Warehouse extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String location;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<InventoryItem> inventoryItems = new ArrayList<>();
+
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.ALL)
+    private List<Shipment> shipments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Staff> staff = new ArrayList<>();
+}
