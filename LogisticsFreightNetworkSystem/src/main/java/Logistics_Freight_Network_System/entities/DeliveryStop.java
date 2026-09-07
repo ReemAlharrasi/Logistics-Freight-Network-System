475f8ea3f69219b46a330a4ea955cd132fd0cbd3
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:35:20 +0400

    created DeliveryStop Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/DeliveryStop.java
@@ -0,0 +1,34 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "delivery_stops")
+public class DeliveryStop extends BaseClass {
+
+    @Column(nullable = false)
+    private Integer sequence;
+
+    @Column(nullable = false, length = 150)
+    private String address;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    @Column(nullable = false)
+    private LocalDateTime eta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "route_id", nullable = false)
+    private Route route;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shipment_id", nullable = false)
+    private Shipment shipment;
+}
