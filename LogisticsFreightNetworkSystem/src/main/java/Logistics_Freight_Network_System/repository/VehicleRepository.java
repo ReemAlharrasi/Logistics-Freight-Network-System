bfea246ff0c81c24a6a489ea5b34f49196c155a4
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 10:46:42 +0400

    created VehicleRepository

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/repository/VehicleRepository.java
@@ -0,0 +1,17 @@
+package Logistics_Freight_Network_System.repository;
+
+import Logistics_Freight_Network_System.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    @Query("SELECT v FROM Vehicle v WHERE v.isActive=true")
+    List<Vehicle> getAllVehicles();
+
+    @Query("SELECT v FROM Vehicle v WHERE v.isActive=true AND v.id=:id")
+    Vehicle getVehicleById(@Param("id") Long id);
+}
