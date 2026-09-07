0e8218cc3c0e7759602cf9b32c5388a21d85dc91
unknown <reemalharrasi00@gmail.com>
2026-09-07 08:49:53 +0400

    spring boot initialized project

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/LogisticsFreightNetworkSystemApplication.java
@@ -0,0 +1,13 @@
+package Logistics_Freight_Network_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsFreightNetworkSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsFreightNetworkSystemApplication.class, args);
+	}
+
+}
---
+++ LogisticsFreightNetworkSystem/src/test/java/Logistics_Freight_Network_System/LogisticsFreightNetworkSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Logistics_Freight_Network_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsFreightNetworkSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
