8d180cb1ffeb67df800998b57b1e74243ed25803
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:37:02 +0400

    created Vehicle Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Vehicle.java
@@ -0,0 +1,34 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "vehicles")
+public class Vehicle extends BaseClass {
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String plateNumber;
+
+    @Column(nullable = false, length = 30)
+    private String type;
+
+    @Column(nullable = false)
+    private Double capacityKg;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carrier_id", nullable = false)
+    private Carrier carrier;
+
+    @OneToMany(mappedBy = "vehicle")
+    private List<Route> routes = new ArrayList<>();
+}
