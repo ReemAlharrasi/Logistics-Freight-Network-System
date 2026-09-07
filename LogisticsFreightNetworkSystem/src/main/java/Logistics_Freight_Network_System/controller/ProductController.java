711e0edc063694dccfdaf60899f2db0964f2e884
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 11:28:19 +0400

    created ProductController

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/controller/ProductController.java
@@ -0,0 +1,46 @@
+package Logistics_Freight_Network_System.controller;
+
+// ---------------- ProductController ----------------
+import Logistics_Freight_Network_System.entity.Product;
+import Logistics_Freight_Network_System.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return productService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        return productService.create(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.update(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void softDelete(@PathVariable Long id) {
+        productService.softDelete(id);
+    }
+}
