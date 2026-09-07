5e00538074a195d61e2fd5dd6c0e4989565646ec
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:35:08 +0400

    created customer Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Customer.java
@@ -0,0 +1,33 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 30)
+    private String type;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Shipment> shipments = new ArrayList<>();
+}
