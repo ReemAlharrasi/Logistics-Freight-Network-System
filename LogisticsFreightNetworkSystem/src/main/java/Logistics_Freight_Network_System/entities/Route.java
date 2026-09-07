27e72e928e818714c2ba358c7f523f83639e207b
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:36:11 +0400

    created Route Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Route.java
@@ -0,0 +1,39 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "routes")
+public class Route extends BaseClass {
+
+    @Column(nullable = false)
+    private LocalDate routeDate;
+
+    @Column(nullable = false, length = 100)
+    private String origin;
+
+    @Column(nullable = false, length = 100)
+    private String destination;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "driver_id", nullable = false)
+    private Driver driver;
+
+    @OneToMany(mappedBy = "route", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DeliveryStop> deliveryStops = new ArrayList<>();
+}
