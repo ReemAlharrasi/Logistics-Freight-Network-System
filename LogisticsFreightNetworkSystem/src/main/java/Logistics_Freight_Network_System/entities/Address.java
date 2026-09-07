038a5a6ff8b8191c0e57a69a93b012af4ebaaa35
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:34:48 +0400

    created Address Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Address.java
@@ -0,0 +1,33 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
+public class Address extends BaseClass {
+
+    @Column(nullable = false, length = 150)
+    private String street;
+
+    @Column(nullable = false, length = 80)
+    private String city;
+
+    @Column(nullable = false, length = 20)
+    private String postalCode;
+
+    @Column(nullable = false, length = 60)
+    private String country;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    // Optional link into a ServiceZone's coverage area
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_zone_id")
+    private ServiceZone serviceZone;
+}
