12e118b55da25792b3d72a2d685757043e435f89
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 10:45:02 +0400

    created AddressRepository

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/repository/AddressRepository.java
@@ -0,0 +1,16 @@
+package Logistics_Freight_Network_System.repository;
+
+import Logistics_Freight_Network_System.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    @Query("SELECT a FROM Address a WHERE a.isActive=true")
+    List<Address> getAllAddresses();
+
+    @Query("SELECT a FROM Address a WHERE a.isActive=true AND a.id=:id")
+    Address getAddressById(@Param("id") Long id);
+}
