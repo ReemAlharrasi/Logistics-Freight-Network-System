116e27b526db273e217d102b08c953f8bb2f5a79
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 10:45:56 +0400

    created RouteRepository

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/repository/RouteRepository.java
@@ -0,0 +1,16 @@
+package Logistics_Freight_Network_System.repository;
+
+import Logistics_Freight_Network_System.entity.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+    @Query("SELECT r FROM Route r WHERE r.isActive=true")
+    List<Route> getAllRoutes();
+
+    @Query("SELECT r FROM Route r WHERE r.isActive=true AND r.id=:id")
+    Route getRouteById(@Param("id") Long id);
+}
