5a438b7d46235353b32c4a56e31cbedb4682cc9c
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:36:40 +0400

    created Staff Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Staff.java
@@ -0,0 +1,25 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "staff")
+public class Staff extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String role;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
+}
