a4356780ec636a920d1622f895d658b8e88624bf
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 10:45:23 +0400

    created DeliveryStopRepository

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/repository/DeliveryStopRepository.java
@@ -0,0 +1,16 @@
+package Logistics_Freight_Network_System.repository;
+
+import Logistics_Freight_Network_System.entity.DeliveryStop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface DeliveryStopRepository extends JpaRepository<DeliveryStop, Long> {
+    @Query("SELECT ds FROM DeliveryStop ds WHERE ds.isActive=true")
+    List<DeliveryStop> getAllDeliveryStops();
+
+    @Query("SELECT ds FROM DeliveryStop ds WHERE ds.isActive=true AND ds.id=:id")
+    DeliveryStop getDeliveryStopById(@Param("id") Long id);
+}
