ad89a85dec7cfcc29a90ba414564f850287dc6cc
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-07 09:35:26 +0400

    created Driver Class

---
+++ LogisticsFreightNetworkSystem/src/main/java/Logistics_Freight_Network_System/entities/Driver.java
@@ -0,0 +1,34 @@
+package Logistics_Freight_Network_System.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "drivers")
+public class Driver extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String licenseNumber;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carrier_id", nullable = false)
+    private Carrier carrier;
+
+    @OneToMany(mappedBy = "driver")
+    private List<Route> routes = new ArrayList<>();
+}
